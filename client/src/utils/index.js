8da8c67fdfb7b7181103e8ed87c0590a8ba269f7
Aman Jindal <amanjindal231@gmail.com>
2023-04-13 01:26:59 +0530

    first commit

---
+++ client/src/utils/index.js
@@ -0,0 +1,16 @@
+import FileSaver from 'file-saver';
+import { surpriseMePrompts } from '../constants';
+
+export function getRandomPrompt(prompt) {
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const randomPrompt = surpriseMePrompts[randomIndex];
+
+    //In case we get the same prompt as previous one
+    if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+    return randomPrompt;
+}
+
+export async function downloadImage(_id, photo) {
+    FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
